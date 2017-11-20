f7d26356e4f6f06a5032676dd5328bef8c4b586f
JianZG <2992265870@qq.com>
2017-11-20 18:17:39 +0800

    ;

---
+++ src/chrome/webpack.config.js
@@ -0,0 +1,58 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var bubleOptions = {
+  target: process.env.NODE_ENV === 'production' ? null : { chrome: 52, firefox: 48 },
+  objectAssign: 'Object.assign'
+}
+
+module.exports = {
+  entry: {
+    'spider-background': './src/spider-background.js'
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  resolve: {
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'buble-loader',
+        exclude: /node_modules|vue\/dist|vuex\/dist/,
+        options: bubleOptions
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          preserveWhitespace: false,
+          buble: bubleOptions
+        }
+      },
+      {
+        test: /\.(png|woff2)$/,
+        loader: 'url-loader?limit=0'
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  devtool: process.env.NODE_ENV !== 'production'
+    ? '#inline-source-map'
+    : false
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
+  ]
+}
